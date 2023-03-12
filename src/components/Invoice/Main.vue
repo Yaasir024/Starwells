696799b52cf9a8ed6af14bcfd2a2949fd81ab7bd
Yaasir024 <yope4ever@gmail.com>
2023-03-12 04:00:04 +0100

    /

---
+++ src/components/Invoice/Main.vue
@@ -0,0 +1,133 @@
+<template>
+  <section class="pt-[105px] px-[20px] sm:px-[35px] lg:px-[60px] xl:px-[120px] pb-[120px]">
+    <div class="">
+      <div class="flex flex-wrap mb-[100px]">
+        <div class="flex-full md:flex-50% pr-[20px] lg:pr-[50px] xl:pr-[100px]">
+          <img src="@/assets/images/invoice/templates.png" alt="" class="" />
+        </div>
+        <div class="flex-full md:flex-50% pl-[20px] lg:pl-[50px] xl:pl-[100px]">
+          <h2
+            class="text-[36px] lg:text-[48px] leading-[42px] lg:leading-[54px] tracking-[-0.02em] font-semibold text-primary-900 mt-[30px] max-w-[480px] mb-[24px]"
+          >
+            Invoice Templates & Payment Reminders
+          </h2>
+          <p
+            class="text-[16px] leading-[26px] tracking-[-0.012em] text-alpha-black/60 mb-[30px] max-w-[460px]"
+          >
+            You can easily create customized templates alongside payment reminders, which will
+            effectively remind your clients of their outstanding payments without you having to
+            manually follow up with them.
+          </p>
+          <ul class="">
+            <li class="flex items-center mb-[24px]">
+              <img src="@/assets/icons/check.svg" alt="" class="mr-[14px]" />
+              <span
+                class="text-[16px] leading-[24px] tracking-[-0.01em] text-primary-900 font-semibold"
+                >Reminder Scheduling</span
+              >
+            </li>
+            <li class="flex items-center mb-[24px]">
+              <img src="@/assets/icons/check.svg" alt="" class="mr-[14px]" />
+              <span
+                class="text-[16px] leading-[24px] tracking-[-0.01em] text-primary-900 font-semibold"
+                >Multiple Payment Options e.g. Credit card, Paypal etc</span
+              >
+            </li>
+            <li class="flex items-center mb-[24px]">
+              <img src="@/assets/icons/check.svg" alt="" class="mr-[14px]" />
+              <span
+                class="text-[16px] leading-[24px] tracking-[-0.01em] text-primary-900 font-semibold"
+                >Multichannel communication</span
+              >
+            </li>
+          </ul>
+        </div>
+      </div>
+      <!--  -->
+      <div class="flex flex-wrap mb-[100px]">
+        <div class="flex-full md:flex-50% pr-[20px] lg:pr-[50px] xl:pr-[100px] order-1 md:order-2">
+          <img src="@/assets/images/invoice/tracking.png" alt="" class="" />
+        </div>
+        <div class="flex-full md:flex-50% pl-[20px] lg:pl-[50px] xl:pl-[100px] order-2 md:order-1">
+          <h2
+            class="text-[36px] lg:text-[48px] leading-[42px] lg:leading-[54px] tracking-[-0.02em] font-semibold text-primary-900 mt-[30px] max-w-[480px] mb-[24px]"
+          >
+            Expense tracking
+          </h2>
+          <p
+            class="text-[16px] leading-[26px] tracking-[-0.012em] text-alpha-black/60 mb-[30px] max-w-[460px]"
+          >
+            Keep track of your expenses easily and accurately, ensuring that you don't miss any
+            deductions come tax time.
+          </p>
+          <ul class="">
+            <li class="flex items-center mb-[24px]">
+              <img src="@/assets/icons/check.svg" alt="" class="mr-[14px]" />
+              <span
+                class="text-[16px] leading-[24px] tracking-[-0.01em] text-primary-900 font-semibold"
+                >Save valuable time by automating your expenses</span
+              >
+            </li>
+            <li class="flex items-center mb-[24px]">
+              <img src="@/assets/icons/check.svg" alt="" class="mr-[14px]" />
+              <span
+                class="text-[16px] leading-[24px] tracking-[-0.01em] text-primary-900 font-semibold"
+                >Minimize errors & reduce the risk of over or underreporting</span
+              >
+            </li>
+            <li class="flex items-center mb-[24px]">
+              <img src="@/assets/icons/check.svg" alt="" class="mr-[14px]" />
+              <span
+                class="text-[16px] leading-[24px] tracking-[-0.01em] text-primary-900 font-semibold"
+                >Minimize errors & reduce the risk of over or underreporting</span
+              >
+            </li>
+          </ul>
+        </div>
+      </div>
+      <!--  -->
+      <div class="flex flex-wrap">
+        <div class="flex-full md:flex-50% pr-[20px] lg:pr-[50px] xl:pr-[100px]">
+          <img src="@/assets/images/invoice/report.png" alt="" class="" />
+        </div>
+        <div class="flex-full md:flex-50% pl-[20px] lg:pl-[50px] xl:pl-[100px]">
+          <h2
+            class="text-[36px] lg:text-[48px] leading-[42px] lg:leading-[54px] tracking-[-0.02em] font-semibold text-primary-900 mt-[30px] max-w-[480px] mb-[24px]"
+          >
+            Reporting and analytics
+          </h2>
+          <p
+            class="text-[16px] leading-[26px] tracking-[-0.012em] text-alpha-black/60 mb-[30px] max-w-[460px]"
+          >
+            You can monitor the effectiveness of your payment reminders through reporting and
+            analytics, giving you insights into client payment behavior and areas where you can
+            improve your invoicing process.
+          </p>
+          <ul class="">
+            <li class="flex items-center mb-[24px]">
+              <img src="@/assets/icons/check.svg" alt="" class="mr-[14px]" />
+              <span
+                class="text-[16px] leading-[24px] tracking-[-0.01em] text-primary-900 font-semibold"
+                >Reminder Scheduling</span
+              >
+            </li>
+            <li class="flex items-center mb-[24px]">
+              <img src="@/assets/icons/check.svg" alt="" class="mr-[14px]" />
+              <span
+                class="text-[16px] leading-[24px] tracking-[-0.01em] text-primary-900 font-semibold"
+                >Multiple Payment Options e.g. Credit card, Paypal etc</span
+              >
+            </li>
+            <li class="flex items-center mb-[24px]">
+              <img src="@/assets/icons/check.svg" alt="" class="mr-[14px]" />
+              <span
+                class="text-[16px] leading-[24px] tracking-[-0.01em] text-primary-900 font-semibold"
+                >Multichannel communication</span
+              >
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
---
+++ src/views/Invoice.vue
@@ -0,0 +1,15 @@
+<script setup>
+import Hero from '@/components/Invoice/Hero.vue'
+import Main from '@/components/Invoice/Main.vue'
+import GrowBusiness from '@/components/Sections/GrowBusiness.vue'
+</script>
+
+<template>
+  <div class="">
+    <Hero />
+    <Main />
+    <div class="mt-[215px] mb-[160px]">
+      <GrowBusiness />
+    </div>
+  </div>
+</template>
